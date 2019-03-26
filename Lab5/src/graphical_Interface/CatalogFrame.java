4c02a6f16891d59085b32c4c434e2bb526c072c0
AndreiG <48528839+AndreiGhiran@users.noreply.github.com>
2019-03-26 21:39:36 +0200

    Lab5 new and improved

--- Lab5/src/graphical_Interface/CatalogFrame.java
+++ Lab5/src/graphical_Interface/CatalogFrame.java
@@ -1,6 +1,6 @@
 package graphical_Interface;
 
-import java.awt.FlowLayout;
+import java.awt.*;
 
 import javax.swing.JFrame;
 
@@ -15,6 +15,32 @@ public class CatalogFrame extends JFrame {
     }
     private void init() {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        this.setSize(565,350);
+    	
+        this.form=new GraphForm(this);
+        this.list=new CatalogList();
+        
+    	c.fill = GridBagConstraints.HORIZONTAL;
+    	c.gridx=0;
+    	c.gridy=0;
+        this.add(form, c);
+        
+    	c = new GridBagConstraints();
+    	this.list.addGraph("CEVA");
+    	c.fill = GridBagConstraints.BOTH;
+    	c.gridx=0;
+    	c.gridy=1;
+    	this.add(list, c);
+    	
+    	c = new GridBagConstraints();
+    	c.fill = GridBagConstraints.HORIZONTAL;
+    	c.gridx=0;
+    	c.gridy=2;
+    	
+    	this.control=new ControlPanel(this);
+    	this.add(control,c);
      //   ...
     }		
    // ...
