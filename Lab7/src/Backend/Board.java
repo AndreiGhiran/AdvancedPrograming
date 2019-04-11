bca56ffb82db4e95fe109ccd4881e32c98f7dea4
cris <cris_st7@yahoo.com>
2019-04-11 20:44:15 +0300

    Board, Edge, Graph methods added.

--- Lab7/src/Backend/Board.java
+++ Lab7/src/Backend/Board.java
@@ -1,4 +1,48 @@
 package Backend;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Board {
+    private Graph completeGraph;
+    private List<Graph> playerGraphs = new ArrayList<>();
+
+    public Board(int numberOdEdges) {
+        List<Edge> generatedEdges = new ArrayList<>();
+        for(int i = 0; i < numberOdEdges; i+=2){
+            generatedEdges.add(new Edge(i, i+1));
+        }
+        completeGraph = new Graph(generatedEdges);
+    }
+
+    public Graph getCompleteGraph() {
+        return completeGraph;
+    }
+
+    public void setCompleteGraph(Graph completeGraph) {
+        this.completeGraph = completeGraph;
+    }
+
+    public List<Graph> getPlayerGraphs() {
+        return playerGraphs;
+    }
+
+    public void setPlayerGraphs(List<Graph> playerGraphs) {
+        this.playerGraphs = playerGraphs;
+    }
+
+    public synchronized Edge extract(int node1, int node2){
+        List<Edge> completeGraphEdges = completeGraph.getEdges();
+        for(Edge edge : completeGraphEdges)
+            if(edge.getNode1() == node1 && edge.getNode2() == node2){
+                completeGraphEdges.remove(edge);
+                completeGraph.setEdges(completeGraphEdges);
+                return edge;
+            }
+        return null;
+    }
+
+    public boolean isEmpty(){
+        return completeGraph.getEdges().isEmpty();
+    }
 }
--- Lab7/src/Backend/Graph.java
+++ Lab7/src/Backend/Graph.java
@@ -1,4 +1,19 @@
 package Backend;
 
+import java.util.List;
+
 public class Graph {
+    private List<Edge> edges;
+
+    public Graph(List<Edge> edges) {
+        this.edges = edges;
+    }
+
+    public List<Edge> getEdges() {
+        return edges;
+    }
+
+    public void setEdges(List<Edge> edges) {
+        this.edges = edges;
+    }
 }
