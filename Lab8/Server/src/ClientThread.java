7b72befea6d69a0d7592df621cf6dde70f8e113a
cris <cris_st7@yahoo.com>
2019-04-24 02:36:32 +0300

    Client Thread added.

--- Lab8/Server/src/ClientThread.java
+++ Lab8/Server/src/ClientThread.java
@@ -1,2 +1,160 @@
-public class ClientThread {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class ClientThread extends Thread {
+    private Socket socket = null;
+    private final SocialNetworkServer server;
+    private String user = new String();
+
+    public ClientThread(SocialNetworkServer server, Socket socket) {
+        this.server = server;
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (true) {
+                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                String request = in.readLine();
+                String response = execute(request);
+                PrintWriter out = new PrintWriter(socket.getOutputStream());
+                out.println(response);
+                if (response.equals("Bye!"))
+                    break;
+                out.flush();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private String execute(String request) {
+        String response = new String();
+        String[] parsedRequest = request.split(" ");
+        switch (parsedRequest[0]) {
+
+            case "register":
+
+                if (parsedRequest.length > 1) {
+                    response = "Incalid name! Name must not contain spaces.";
+                    break;
+                } else if (parsedRequest.length == 1) {
+                    response = "Please specify a name.";
+                    break;
+                } else {
+                    for (String name : server.usersAndFriends.keySet())
+                        if (name.equals(parsedRequest[1])) {
+                            response = "Invalid name! Name already used.";
+                            break;
+                        }
+                }
+
+                server.usersAndFriends.put(parsedRequest[1], new ArrayList<>());
+                response = "User" + parsedRequest[1] + " created.";
+
+            case "login":
+                if (parsedRequest.length < 2) {
+                    response = "Please specify a name";
+                    break;
+                } else {
+                    for (String name : server.usersAndFriends.keySet())
+                        if (name.equals(parsedRequest[1])) {
+                            response = "Welcome!";
+                            user = name;
+                            break;
+                        } else {
+                            response = "The specified name does not exist.";
+                            break;
+                        }
+                }
+
+            case "friend":
+                if (user.isEmpty()) {
+                    response = "Please log in.";
+                    break;
+                } else if (parsedRequest.length < 2) {
+                    response = "Please specify the name of friend/friends to be added.";
+                    break;
+                } else {
+                    for (int i = 1; i < parsedRequest.length; i++)
+                        for (String name : server.usersAndFriends.keySet())
+                            if (parsedRequest[i].equals(name)) {
+                                List<String> friends = server.usersAndFriends.get(user);
+                                friends.add(parsedRequest[i]);
+                                server.usersAndFriends.put(user, friends);
+                                response = response + "Friend " + parsedRequest[i] + " added.\n";
+                                break;
+                            } else {
+                                response = response + "User " + parsedRequest[i] + " does not exist.\n";
+                            }
+                    break;
+                }
+
+            case "send":
+                if (user.isEmpty()) {
+                    response = "Please log in.";
+                    break;
+                } else if (parsedRequest.length < 2) {
+                    response = "Please specify a message.";
+                    break;
+                } else {
+                    List<String> friends = new ArrayList<>();
+
+                    for (String name : server.usersAndFriends.keySet())
+                        if (name.equals(user))
+                            friends = server.usersAndFriends.get(name);
+
+                    for (String friend : server.usersAndFriends.get(user)) {
+
+                        HashMap<String, String> friendUser = new HashMap<>();
+                        friendUser.put(friend, user);
+                        String oldMessage = server.messages.get(friendUser);
+
+                        for (int i = 1; i < parsedRequest.length; i++)
+                            oldMessage = oldMessage + " " + parsedRequest[i];
+                        oldMessage += "\n";
+
+                        server.messages.put(friendUser, oldMessage);
+                        response = response + "Message sent to " + friend + ".\n";
+                        break;
+                    }
+                }
+
+            case "read":
+                if (user.isEmpty()) {
+                    response = "Please log in.";
+                    break;
+                } else {
+                    List<String> friendList = server.usersAndFriends.get(user);
+                    for (String friend : friendList) {
+                        HashMap<String, String> friendUser = new HashMap<>();
+                        friendUser.put(user, friend);
+                        response = response + "Message from " + friend + ": \n" + server.messages.get(friendUser) + "\n";
+                    }
+                    break;
+                }
+
+            case "exit":
+                response = "Bye!";
+                break;
+
+            default:
+                response = "Invalid command.";
+        }
+
+        return response;
+    }
 }
--- Lab8/Server/src/SocialNetworkServer.java
+++ Lab8/Server/src/SocialNetworkServer.java
@@ -1,6 +1,31 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.HashMap;
+import java.util.List;
+
 public class SocialNetworkServer {
+    private static final int PORT = 8100;
+    private ServerSocket serverSocket;
+    private boolean running = false;
+    public HashMap<String, List<String>> usersAndFriends = new HashMap<>();
+    public HashMap<HashMap<String, String>, String > messages = new HashMap<>();
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        SocialNetworkServer server = new SocialNetworkServer();
+        server.init();
+        server.waitForClients();
+    }
+
+    private void init(){
+
+    }
+
+    private void waitForClients(){
+
+    }
+
+    public void stop() throws IOException{
+        this.running = false;
+        serverSocket.close();
     }
 }
