a55c758bdfa5bed9265dc8d4db1b8e882755831d
Cristian Tugui <cris_st7@yahoo.com>
2019-05-08 13:50:37 +0300

    Main added.

--- Lab9/src/Controllers/PersonController.java
+++ Lab9/src/Controllers/PersonController.java
@@ -10,7 +10,7 @@ public class PersonController {
 
     public void create(String name) throws SQLException {
         Connection con = Database.getConnection();
-        try (PreparedStatement pstmt = con.prepareStatement("INSERT INTO persons values (?, ?)")) {
+          try (PreparedStatement pstmt = con.prepareStatement("INSERT INTO persons values (?, ?)")) {
             pstmt.setInt(1, IdGenerator);
             pstmt.setString(2, name);
             pstmt.executeUpdate();
@@ -25,7 +25,6 @@ public class PersonController {
              ResultSet rs = stmt.executeQuery("SELECT id FROM persons WHERE name LIKE '" + name + "'")) {
             return rs.next() ? rs.getInt(1) : null;
         }
-        con.close();
     }
 
     public String findByID(int id) throws SQLException {
@@ -34,6 +33,5 @@ public class PersonController {
              ResultSet rs = stmt.executeQuery("SELECT name FROM persons WHERE id = '" + id + "'")) {
             return rs.next() ? rs.getString(2) : null;
         }
-        con.close();
     }
 }
--- Lab9/src/Main/Main.java
+++ Lab9/src/Main/Main.java
@@ -1,8 +1,31 @@
 package Main;
 
+import Controllers.MovieController;
+import Controllers.PersonController;
+import Database.Database;
+
+import java.sql.SQLException;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        try {
+            PersonController persons = new PersonController();
+            MovieController movies = new MovieController();
+
+            persons.create("Francis Ford Coppola");
+            persons.create("Marlon Brando");
+            persons.create("Al Pacino");
+            Database.commit();
+
+            movies.create("The Godfather", persons.findByName("Coppola"));
+            //Add other movies to the database
+            Database.commit();
+
+            Database.closeConnection();
+        } catch (SQLException e) {
+            System.err.println(e);
+            Database.rollback();
+        }
     }
 }
