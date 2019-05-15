70a6522c509ccd991c89b4b981b9071ba8de7cb8
AndreiG <48528839+AndreiGhiran@users.noreply.github.com>
2019-05-15 19:18:43 +0300

    adaugat chestii in clase

--- Lab11/src/DynamicDesigner/MainFrame.java
+++ Lab11/src/DynamicDesigner/MainFrame.java
@@ -1,8 +1,26 @@
 package DynamicDesigner;
 
-public class MainFrame {
+import javax.swing.JFrame;
 
+public class MainFrame extends JFrame{
+
+	ControlPanel controlPanel;
+    DesignPanel designPanel;
+    
+    public MainFrame() {
+        super("Swing Designer");
+        init();
+    }
+    private void init() {
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ...//create and add to the frame the controlPanel and designPanel objects		
+        pack();
+    }
+    ...//create the main method
+	
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        MainFrame MF = new MainFrame();
+        MF.setVisible(true);
     }
 }
